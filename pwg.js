e8ec379ccd082aabb4b3e0183eaba11ceab6f6ef
mmoritz8 <mmoritz@stedwards.edu>
2020-02-19 15:03:16 -0600

    "displays password length based on slider"

--- pwg.js
+++ pwg.js
@@ -1,49 +1,21 @@
-var letterCase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-var lowerC = "abcdefghijklmnopqrstuvwxyz";
-var upperC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberC = "1234567890";
-var specialC = "@#$%^&*(){}[]-_+=";
+function generate() {
 
-var values = letterCase;
 
-  function generate(){    
+  // set pw length
 
-    // set pw length
+  var complexity = document.getElementById("slider").value;
+  let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()";
 
-    var complexity = document.getElementById("slider").value;
+  var password = "";
 
-    // setting an array to the complexity measures
+  // create a loop
 
-    console.log(lowerC);
-    console.log(upperC);
-    console.log(numberC);
-    console.log(specialC);
-    console.log(values);
+  for (var i = 0; i <= complexity; i++) {
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  }
 
-    var password = "";
+  document.getElementById("display").value = password;
+};
 
-    // create a loop
-
-    for (var i = 0; i<= complexity.length; i++) {
-      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-
-    }
-
-    document.getElementById("password").value = password;
-    document.getElementById("lastPass").innerHTML += password + "<br>";
-}// add pw to textbox
-
-// default length display slider
-
-    document.getElementById("length").innerHTML = "Length: 2";
-
-    document.getElementById("slider") = slider;
-    slider.oninput = function() {
-
-      if (slider.value > 0) {
-        document.getElementById("length").innerHTML = "Length: " + slider.value;
-      } else {
-        document.getElementById("length").innerHTML = "Length: 1 ";
-      }
-    }
+generate();
